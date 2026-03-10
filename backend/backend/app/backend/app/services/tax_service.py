a833bfd2b413c35897599df711fcc1e8c8512b4c
Javin-states <109912508+Javin-states@users.noreply.github.com>
2026-03-10 16:41:01 +0300

    Update tax_service.py

--- backend/backend/app/backend/app/services/tax_service.py
+++ backend/backend/app/backend/app/services/tax_service.py
@@ -1,1 +1,15 @@
+def calculate_paye(salary):
 
+    if salary <= 235000:
+        tax = 0
+
+    elif salary <= 335000:
+        tax = (salary - 235000) * 0.1
+
+    elif salary <= 410000:
+        tax = 10000 + (salary - 335000) * 0.2
+
+    else:
+        tax = 25000 + (salary - 410000) * 0.3
+
+    return round(tax,2)
