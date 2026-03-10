c35f144e8a8e0bc48c8ec0c210dc730abb4e3e95
Javin-states <109912508+Javin-states@users.noreply.github.com>
2026-03-10 16:39:24 +0300

    Update database.py

--- backend/backend/app/backend/app/backend/app/database.py
+++ backend/backend/app/backend/app/backend/app/database.py
@@ -1,1 +1,13 @@
+from google.cloud import firestore
 
+db = firestore.Client()
+
+def save_question(user_id, question, answer):
+
+    data = {
+        "user_id": user_id,
+        "question": question,
+        "answer": answer
+    }
+
+    db.collection("questions").add(data)
